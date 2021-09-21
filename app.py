ff34165d7e3aa81f34393dc8d5ff8bd9b980bf8e
couchjanus <couchjanus@gmail.com>
2021-09-21 11:48:25 +0300

    added unit 1

---
+++ app.py
@@ -0,0 +1,24 @@
+# comment
+
+a = 3
+b = 3.5
+c = "Hello"
+
+A = 77
+
+# _x = 77777
+a_b = "a and b"
+
+print(c, " World ", A)
+
+print (a / b)
+
+o = '+'
+
+if o == '+':
+    print(a + b)
+elif o == '-':
+    print(a - b)
+else:
+    print("bad operation")
+
