53a9d6318e74344daf75c6c49d9fc828c76cf4c2
couchjanus <couchjanus@gmail.com>
2021-09-22 12:01:57 +0300

    added unit 2

--- app.py
+++ app.py
@@ -1,24 +1,49 @@
 # comment
+title = 'Smart Calculator'
 
-a = 3
-b = 3.5
-c = "Hello"
+running = True
 
-A = 77
+def hello(name):
+    print("It's me, ", name)
+    h = input("Can I help You? (y,n): ")
+    return h
 
-# _x = 77777
-a_b = "a and b"
-
-print(c, " World ", A)
-
-print (a / b)
-
-o = '+'
-
-if o == '+':
-    print(a + b)
-elif o == '-':
-    print(a - b)
-else:
-    print("bad operation")
-
+while running:
+    if hello(title) == 'n':
+        print("THANK YOU FOR USING " + title )
+        break
+    op = input('Enter operation: ')
+    if op == 'q':
+        print("Programm done")
+        running = False
+    else:  
+        a = input('Enter a = ')
+        if a.isdigit():
+            a = float(a)
+        else:
+            print("a shuold be digit")
+            running = False
+            break
+        b = input('Enter b = ')
+        if b.isdigit():
+            b = float(b)
+        else:
+            print("b shuold be digit")
+            running = False
+            break
+        # if op == 'q':
+        #     print("Programm done")
+        #     running = False
+        if op == '+':
+            print('a + b = ', a + b )
+        elif op == '-':
+            print('a - b = ', a - b )
+        elif op == '*':
+            print('a * b = ', a * b )
+        elif op == '/':
+            if b != 0:
+                print('a / b = ', a / b )
+            else:
+                print('Oops, float division by zero')
+        else:
+            print("I don't know what You want")
